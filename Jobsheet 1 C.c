4ec7b9be2de68bb80cf6cbb8baa2ab5d6b3ccbe3
zulkipli-312 <64574762+zulkipli-312@users.noreply.github.com>
2020-05-14 21:02:48 +0800

    Create Jobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main () {
+	printf("Nama : Zulkipli \n");
+	printf("Nim :F1B019149 \n");
+	printf("Kelompok : 29 \n");
+	int myarray[5]={9,8,1,4,0};
+	printf("Data ke-1 = %d",myarray[0]);
+	printf("Data ke-2 = %d",myarray[1]);
+	printf("Data ke-3 = %d",myarray[2]);
+	printf("Data ke-4 = %d",myarray[3]);
+
+}
